535eefe4ca429b74f579b2c12cae41259c471722
AamirL1011 <aamirs@students.cs.ubc.ca>
2021-12-26 23:00:06 -0800

    update

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import theme from "./themes";
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import {Button} from "@material-ui/core";
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 
 
 const useStyles = makeStyles((theme) => ({
@@ -42,18 +43,18 @@ function App(props) {
           <Grid>
             {
                 !mainState && (
-                      <Grid container justifyContent={"center"} alignItems={"center"} style={{backgroundColor: "white", width: "100vw", height: "100vh"}}>
+                      <Grid container justifyContent={"center"} alignItems={"center"} style={{backgroundColor: "lightgray", width: "100vw", height: "100vh"}}>
                         <video playsInline autoPlay muted loop>
-                          <source src={process.env.PUBLIC_URL + "/Assets/Videos/video2.mp4"} type="video/mp4" />
+                          <source src={process.env.PUBLIC_URL + "/Assets/Videos/video.mp4"} type="video/mp4" />
                         </video>
 
                         <Grid container item xs={11} md={5} lg={2} xl={1} style={{zIndex: "50"}} justifyContent={"center"} alignItems={"center"}>
                           <Grid container item justifyContent={"center"} alignItems={"center"} >
                             <Grid container item>
                               <Box sx={{ flexGrow: 1, zIndex: "55", marginBottom: "-5%"}}>
-                                <Grid container spacing={2} justifyContent={"center"} alignItems={"center"} className={""}>
+                                <Grid container spacing={1} justifyContent={"center"} alignItems={"center"}>
                                   <Grid item xs={4}>
-                                    <Paper style={{background: "rgba(158, 155, 147, 0.9)", padding: "5%", textAlign: "center"}} alignItems={"center"} justifyContent={"center"}>
+                                    <Paper style={{background: "rgba(158, 155, 147, 0.9)", padding: "5%", paddingBottom: "3%", textAlign: "center"}} alignItems={"center"} justifyContent={"center"} elevation={4}>
                                       <img src={process.env.PUBLIC_URL + "/Assets/Images/personal_logo3.png"} style={{width: "100%", height: "100%"}}/>
                                     </Paper>
                                   </Grid>
@@ -62,22 +63,21 @@ function App(props) {
                             </Grid>
 
                             <Grid container item className="GreetingBox">
-                              <Box sx={{ flexGrow: 1, background: "rgba(227, 207, 161, 0.9)", padding: "5%", borderRadius: "10px" }}>
+                              <Box sx={{ flexGrow: 1, background: "rgba(227, 207, 161, 0.9)", padding: "5%", borderRadius: "10px", boxShadow: "0px 0.5px 1px grey" }}>
                                 <Grid container spacing={2} className={""}>
                                   <Grid item xs={12}></Grid>
                                   <Grid item xs={12}></Grid>
                                   <Grid item xs={12} className={"typewriter"}>
-                                    <Paper>
-                                      Secret Message:
-                                      <p className={"line anim-typewriter"}
+                                    <Paper style={{padding: "2%"}}>
+                                      <p style={{color: "gray", fontSize: "1.0em"}} className={"line anim-typewriter"}
                                       >
-                                        Welcome to my page!
+                                        I'm Aamir, welcome to my page :)
                                       </p>
                                     </Paper>
                                   </Grid>
                                   <Grid item xs={4} />
                                   <Grid item xs={4} alignItems={"center"} justifyContent={"center"} style={{textAlign: "center"}}>
-                                    <Button>
+                                    <Button variant="outlined" endIcon={<ArrowForwardIosIcon />} style={{textTransform: "none"}}>
                                       Enter
                                     </Button>
                                   </Grid>
