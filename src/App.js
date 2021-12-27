40182f1eaac3b1c7c23f3f61e49b0bfee6394527
AamirL1011 <aamirs@students.cs.ubc.ca>
2021-12-26 18:24:09 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ import { connect } from "react-redux";
 import {updateMainState} from "./Actions/mainActions";
 import { makeStyles } from '@material-ui/core/styles';
 import theme from "./themes";
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import {Button} from "@material-ui/core";
 
 
 const useStyles = makeStyles((theme) => ({
@@ -16,7 +19,7 @@ const useStyles = makeStyles((theme) => ({
     alignItems: "center",
   },
   content: {
-    opacity: "100%"
+    opacity: "100% !important"
   }
 }));
 
@@ -39,30 +42,45 @@ function App(props) {
           <Grid>
             {
                 !mainState && (
-                    <Grid style={{backgroundColor: "white", width: "100vw", height: "100vh"}}>
-                      <Grid container justifyContent={"center"} alignItems={"center"}>
+                      <Grid container justifyContent={"center"} alignItems={"center"} style={{backgroundColor: "white", width: "100vw", height: "100vh"}}>
                         <video playsInline autoPlay muted loop>
                           <source src={process.env.PUBLIC_URL + "/Assets/Videos/video2.mp4"} type="video/mp4" />
                         </video>
 
                         <Grid container item xs={12} md={6} style={{zIndex: "50"}} justifyContent={"center"} alignItems={"center"}>
-                          <Grid container item justify={"space-evenly"} alignItems={"center"} >
-                            <Grid container item xs={12} justify={"center"} alignItems={"center"} style={{marginTop: "5%"}}>
-                              <Grid item xs={3}>
-                                <img src={process.env.PUBLIC_URL + "/Assets/Images/personal_logo3.png"} style={{width: "100%", height: "100%"}}/>
-                              </Grid>
-                            </Grid>
-                            <Grid xs item className={"typewriter"}>
-                              <p className={"line anim-typewriter"}
-                              >
-                                Welcome!
-                              </p>
+                          <Grid container item justifyContent={"center"} alignItems={"center"} >
+                            <Grid container item className="GreetingBox">
+                              <Box sx={{ flexGrow: 1, background: "rgba(158, 155, 147, 0.8)", padding: "5%", borderRadius: "10px" }}>
+                                <Grid container spacing={2} className={""}>
+                                  <Grid item xs={4}/>
+                                  <Grid item xs={4}>
+                                    <Paper style={{background: "rgba(245, 228, 181, 0.8)", padding: "10px", textAlign: "center"}} alignItems={"center"} justifyContent={"center"}>
+                                      <img src={process.env.PUBLIC_URL + "/Assets/Images/personal_logo3.png"} style={{width: "90%", height: "90%"}}/>
+                                    </Paper>
+                                  </Grid>
+                                  <Grid item xs={4}/>
+                                  <Grid item xs={12} className={"typewriter"}>
+                                    <Paper>
+                                      <p className={"line anim-typewriter"}
+                                      >
+                                        Welcome!
+                                      </p>
+                                    </Paper>
+                                  </Grid>
+                                  <Grid item xs={4} />
+                                  <Grid item xs={4} alignItems={"center"} justifyContent={"center"} style={{textAlign: "center"}}>
+                                    <Button>
+                                      Enter
+                                    </Button>
+                                  </Grid>
+                                  <Grid item xs={4} />
+                                </Grid>
+                              </Box>
                             </Grid>
                           </Grid>
                         </Grid>
 
                       </Grid>
-                    </Grid>
                 )
             }
             {
