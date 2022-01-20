11356f01eaa7af4847f5f8ab776fb42f8bab8af2
AamirL1011 <aamirs@students.cs.ubc.ca>
2022-01-19 23:07:51 -0800

    Splash page updates

--- src/App.js
+++ src/App.js
@@ -14,6 +14,9 @@ import {Button} from "@material-ui/core";
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import Backdrop from '@mui/material/Backdrop';
 import CircularProgress from '@mui/material/CircularProgress';
+import CircleIcon from '@mui/icons-material/Circle';
+import { Fade } from "react-awesome-reveal";
+
 
 
 const useStyles = makeStyles((theme) => ({
@@ -72,43 +75,63 @@ function App(props) {
                             <source src={process.env.PUBLIC_URL + "/Assets/Videos/video.mp4"} type="video/mp4" />
                           </video>
 
-                          <Grid container item xs={11} md={5} lg={2} xl={1} style={{zIndex: "50"}} justifyContent={"center"} alignItems={"center"}>
+                          <Grid container item xs={11} sm={7} md={5} lg={2} xl={1} style={{zIndex: "50"}} justifyContent={"center"} alignItems={"center"}>
                             <Grid container item justifyContent={"center"} alignItems={"center"} >
                               <Grid container item>
-                                <Box sx={{ flexGrow: 1, zIndex: "55", marginBottom: "-5%"}}>
-                                  <Grid container spacing={1} justifyContent={"center"} alignItems={"center"}>
-                                    <Grid item xs={4}>
-                                      <Paper style={{background: "rgba(158, 155, 147, 0.9)", padding: "5%", paddingBottom: "3%", textAlign: "center"}} elevation={4}>
-                                        <img src={process.env.PUBLIC_URL + "/Assets/Images/personal_logo3.png"} style={{width: "100%", height: "100%"}}/>
-                                      </Paper>
-                                    </Grid>
-                                  </Grid>
+                                <Box sx={{ flexGrow: 1, zIndex: "55", marginBottom: "5%"}}>
+                                    <Fade delay={1000} triggerOnce={true}>
+                                        <Grid container spacing={1} justifyContent={"center"} alignItems={"center"}>
+                                            <Grid item xs={4}>
+                                                <Paper style={{background: "rgba(158, 155, 147, 0.9)", padding: "5%", paddingBottom: "3%", textAlign: "center"}} elevation={4}>
+                                                    <img src={process.env.PUBLIC_URL + "/Assets/Images/personal_logo3.png"} style={{width: "100%", height: "100%"}}/>
+                                                </Paper>
+                                            </Grid>
+                                        </Grid>
+                                    </Fade>
                                 </Box>
                               </Grid>
 
                               <Grid container item className="GreetingBox">
-                                <Box sx={{ flexGrow: 1, background: "rgba(227, 207, 161, 0.9)", padding: "5%", borderRadius: "10px", boxShadow: "0px 0.5px 1px grey" }}>
-                                  <Grid container spacing={2} className={""}>
-                                    <Grid item xs={12}></Grid>
-                                    <Grid item xs={12}></Grid>
-                                    <Grid item xs={12} className={"typewriter"}>
+                                <Box sx={{ flexGrow: 1, background: "rgba(227, 207, 161, 0.7)", backdropFilter: "blur(3px)", padding: "3%", borderRadius: "10px", boxShadow: "0px 0.5px 1px grey" }}>
+                                  <Grid container spacing={2}>
+                                      <Grid item xs={12}>
+                                          <Grid container direction={"row"} justifyContent={"space-evenly"} alignItems={"center"} >
+                                              <Grid item xs={3} sm={2} style={{marginTop: "-1.5%", marginLeft: "-1.5%"}}>
+                                                  <CircleIcon className={"menuButton"} onClick={() => handleEnter()}  style={{color: "rgba(255,69,58,0.95)", fontSize: "15px", paddingRight: "5px", cursor: "pointer"}} />
+                                                  <CircleIcon className={"menuButton"} onClick={() => handleEnter()} style={{color: "rgba(255,186,10,0.95)", fontSize: "15px", paddingRight: "5px", cursor: "pointer"}} />
+                                                  <CircleIcon className={"menuButton"} onClick={() => handleEnter()} style={{color: "rgba(50,215,75,0.95)", fontSize: "15px", cursor: "pointer"}} />
+                                              </Grid>
+                                              <Grid item xs={9} sm={10} />
+                                          </Grid>
+                                      </Grid>
+                                    <Grid item xs={12} >
                                       <Paper style={{padding: "2%"}}>
-                                        <p style={{color: "gray", fontSize: "1.0em"}} className={"line anim-typewriter"}
-                                        >
-                                          I'm Aamir, welcome to my page :)
-                                        </p>
+                                          <Grid container item direction={"row"} justifyContent={"space-evenly"} alignItems={"center"}>
+                                            <Grid item xs={1} sm={1} style={{marginTop: "-3px"}}>
+                                                <span style={{color: "gray", fontSize: "1.3em", fontWeight: "500",
+                                                    fontFamily: "-apple-system, Roboto, serif"}}>>_ </span>
+                                            </Grid>
+                                            <Grid item xs={11} sm={11} className={"typewriter"} style={{textAlign: "start"}}>
+                                                <p style={{color: "gray", fontSize: "1.0em"}} className={"line anim-typewriter"}
+                                                >
+                                                    I'm Aamir, welcome to my page :)
+                                                </p>
+                                            </Grid>
+                                          </Grid>
                                       </Paper>
                                     </Grid>
                                     <Grid item xs={4} />
-                                    <Grid item xs={4} style={{textAlign: "center"}}>
+                                      <Grid item xs={4} />
+                                    <Grid item xs={4} style={{textAlign: "end"}}>
                                       <Button variant="outlined" endIcon={<ArrowForwardIosIcon />}
-                                              style={{textTransform: "none"}}
+                                              size={"small"}
+                                              style={{textTransform: "none", backgroundColor: "whitesmoke",
+                                                  borderRadius: "4px", fontFamily: "-apple-system, Roboto, serif"}}
                                               className={`enter-button ${classes.enterButton}`}
                                               onClick={() => handleEnter()}>
                                         Enter
                                       </Button>
                                     </Grid>
-                                    <Grid item xs={4} />
                                   </Grid>
                                 </Box>
                               </Grid>
